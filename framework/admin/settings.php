2141d6d1f5c345cda5a9dabd77aed611f94e835d
Thesabel Tuto <thesabeltuto@gmail.com>
2016-07-02 04:38:50 +0800

    1.0.5

--- framework/admin/settings.php
+++ framework/admin/settings.php
@@ -7,7 +7,7 @@ function create_admin_menu_geophy() {
 }
 
 function salient_geophy_css() {
-	wp_register_style('salient_geophy_css.css', THEME_URL.'/framework/admin/admin.css', '', $GLOBALS['THEME_CSS_VERSION'], '');
+	wp_register_style('salient_geophy_css.css', THEME_CHILD_URL.'/framework/admin/admin.css', '', $GLOBALS['THEME_CSS_VERSION'], '');
 	wp_enqueue_style('salient_geophy_css.css');
 }
 ?>
